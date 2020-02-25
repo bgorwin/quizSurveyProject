fa3b0757b5c7960b73f69e52921bae1958e093b8
Brent Gorwin <brent.gorwin@gmail.com>
2020-02-24 19:57:09 -0600

    Added the javascript .map for the questions and styled using CSS. Next going to add in the answers array for user to select.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,34 @@
-console.log('This is connected');
+const questions = [
+    {
+        question1: 'What does CSS stand for?',
+        answers: {
+            a: 'Cascade Style Sheets',
+            b: 'Cool Style Syntax',
+            c: 'Case Sensitive Subjects'
+        }
+    },
+    {
+        question2: 'One use of Javascript is to ______.',
+        answers: {
+            a: 'Code in Java',
+            b: 'Build blocks of reusable code.',
+            c: 'Change the text font style.'
+        }
+    }
+]
+
+// Map through the questions array object.
+const listOfQuestions = (question, answer) => {
+    const mapThroughQuestions = [question.question1, answer.answers].join('');
+    return mapThroughQuestions;
+}
+
+document.getElementById("questionsDisplay").innerHTML = questions.map(listOfQuestions);
+
+
+
+// Select answer
+
+
+
+// Submit Answer
